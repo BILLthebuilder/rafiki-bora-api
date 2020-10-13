f4085a05a371208b10793ad79375e9a9addab651
1sarah <mbuyaeugenis@gmail.com>
2020-10-13 14:22:30 +0300

    list according to role name

--- src/main/java/rafikibora/repository/RoleRepository.java
+++ src/main/java/rafikibora/repository/RoleRepository.java
@@ -5,6 +5,7 @@ import rafikibora.model.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface RoleRepository extends JpaRepository<Roles, Long>{
     Optional<Roles> findByRoleName(String name);
