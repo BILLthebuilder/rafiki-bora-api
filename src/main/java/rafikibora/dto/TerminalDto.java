74d6692249a130785941585e1e26785c7302f85d
Michelle <michysese@gmail.com>
2020-10-13 17:35:05 +0300

    Terminal new modification

--- src/main/java/rafikibora/dto/TerminalDto.java
+++ src/main/java/rafikibora/dto/TerminalDto.java
@@ -1,6 +1,15 @@
 package rafikibora.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TerminalDto {
+
     String modelType;
     String serialNumber;
     String Id;
@@ -9,28 +18,4 @@ public class TerminalDto {
 //    String Status;
 //    String DateUpdated;
 
-
-    public String getModelType() {
-        return modelType;
-    }
-
-    public void setModelType(String modelType) {
-        this.modelType = modelType;
-    }
-
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    public void setSerialNumber(String serialNumber) {
-        this.serialNumber = serialNumber;
-    }
-
-    public String getId() {
-        return Id;
-    }
-
-    public void setId(String id) {
-        Id = id;
-    }
 }
--- src/main/java/rafikibora/services/TerminalService.java
+++ src/main/java/rafikibora/services/TerminalService.java
@@ -6,13 +6,18 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+import rafikibora.dto.CustomUserDetails;
 import rafikibora.dto.TerminalDto;
 import rafikibora.model.terminal.Terminal;
+import rafikibora.model.users.User;
 import rafikibora.repository.TerminalRepository;
 
 import javax.transaction.Transactional;
+import java.security.Principal;
 import java.util.*;
 
 
@@ -22,22 +27,26 @@ public class TerminalService implements TerminalInterface {
     @Autowired
     private TerminalRepository terminalRepository;
 
+    //Create Tid
+    public String createTID(){
+        return UUID.randomUUID().toString().substring(0,16);
+    }
 
     //Create Terminal
     @Transactional
-    public void save(TerminalDto terminalDto) {
-        Terminal terminal = new Terminal();
-        terminal.setModelType(terminalDto.getModelType());
-        terminal.setSerialNo(terminalDto.getSerialNumber());
+    public Terminal save(Terminal terminal) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
+//      Terminal terminal = new Terminal();
+        terminal.setModelType(terminal.getModelType());
+        terminal.setSerialNo(terminal.getSerialNo());
         terminal.setDeleted(false);
         terminal.setDateCreated(new Date());
         terminal.setDateUpdated(new Date());
         terminal.setStatus(true);
-        terminal.setTid("jfhthakj");
-        terminal.setTid("ghtyg");
-        terminal.setTid("tttt");
-        terminalRepository.save(terminal);
-
+        terminal.setTid(createTID());
+        terminal.setTerminalMaker(user.getUser());
+        return terminalRepository.save(terminal);
     }
 
     //List All Terminals
