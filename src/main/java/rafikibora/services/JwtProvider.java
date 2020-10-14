d4812d7a6962da48b8c0b3c3a0bc7ad318411870
wangecimeruh <40371297+wangecimeruh@users.noreply.github.com>
2020-10-14 19:51:14 +0300

    Update JWT provider

--- src/main/java/rafikibora/services/JwtProvider.java
+++ src/main/java/rafikibora/services/JwtProvider.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import rafikibora.exceptions.InvalidTokenException;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
