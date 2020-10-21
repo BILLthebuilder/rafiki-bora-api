d58dee6e381f174f0760fac32059d863f2f113fa
Michelle <michysese@gmail.com>
2020-10-21 10:55:46 +0300

    comments

--- src/main/java/rafikibora/services/TerminalService.java
+++ src/main/java/rafikibora/services/TerminalService.java
@@ -66,14 +66,17 @@ public class TerminalService implements TerminalInterface {
         terminal.setCreatedOn((LocalDateTime.now()));
         terminal.setUpdatedOn((LocalDateTime.now()));
         terminal.setTid(createTID());
-//        terminal.setMid(user.getUser());
+        terminal.setMid(user.getUser());
 //        terminal.setMid(user.getUser());
 //        terminal.setMid(createMID());
         terminal.setTerminalMaker(user.getUser());
 //        return terminalRepository.save(terminal);
 
 
-        //Catching Entry of Duplicate Values.
+
+        /**
+         Catching Entry of Duplicate Values.
+         */
 
         try {
             terminalRepository.save(terminal);
