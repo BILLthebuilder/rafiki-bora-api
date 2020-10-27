d7ecb51dc4b96960cd71d570f0b9cf20750da81b
wangecimeruh <40371297+wangecimeruh@users.noreply.github.com>
2020-10-27 19:00:35 +0300

    created the list method by roles

--- src/main/java/rafikibora/services/UserServiceI.java
+++ src/main/java/rafikibora/services/UserServiceI.java
@@ -2,6 +2,7 @@ package rafikibora.services;
 
 import org.springframework.http.ResponseEntity;
 import rafikibora.dto.*;
+import rafikibora.model.account.Account;
 import rafikibora.model.terminal.Terminal;
 import rafikibora.model.users.User;
 
@@ -12,7 +13,7 @@ public interface UserServiceI {
 
     ResponseEntity<AuthenticationResponse> login(LoginRequest loginRequest) throws Exception;
 
-    Set<User> getUserByRole(String roleName);
+    List<User> getUserByRole(String roleName);
 
     User findByName(String name);
 
